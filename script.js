2e22e19186ed6cd7ed6ef0a41e82b0424bb112c6
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2024-08-12 21:45:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const para = document.getElementById("status");
-const body = document.getElementsByTagName("body")[0];
 
-function changeText(params) {
-    const newH = document.createElement("h1");
-    newH.id = "status";
-    newH.innerText = "Entered Metaverse";
-    para.replaceWith(newH);
+function changeText() {
+    para.innerHTML = "<h1>Entered Metaverse</h1>";
 }
